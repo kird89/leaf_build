867e1e9cee35ac82f927bc20a00c630852a49487
SamarV-121 <samar@samarv121.dev>
2023-06-05 12:19:27 +0530

    jenkins: Improve repo sync parallel jobs
     * Use total number of threads for local checkout
     * Limit threads to 16 for network jobs
    
    Change-Id: I284eda41ec146af1116a3d37a24a7beacafbcc7c

--- jenkins/leaf-build.sh
+++ jenkins/leaf-build.sh
@@ -52,10 +52,11 @@ function init() {
 }
 
 function sync() {
+	THREADS=$(nproc)
 	telegram editMessageText "$TELEGRAM_MESSAGE Syncing"
 	repo init -u https://git.leafos.org/LeafOS-Project/android -b "leaf-$JENKINS_LEAF_VERSION" --depth=1
 	repo forall -c 'git reset --hard; git clean -fdx' >/dev/null || true
-	repo sync -j12 --force-sync
+	repo sync -j"$THREADS" --jobs-network=$((THREADS < 16 ? THREADS : 16)) --force-sync
 	if [ "$JENKINS_REPOPICK" ] && [ "$JENKINS_RELEASETYPE" != stable ]; then
 		read -a JENKINS_REPOPICK <<<"$JENKINS_REPOPICK"
 		for change in "${JENKINS_REPOPICK[@]}"; do
